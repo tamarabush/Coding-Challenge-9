bbfb4636017e286b71aa0d3282ab12858a7a7db2
tamarabush <tamara.abush14@outlook.com>
2024-10-18 13:02:38 -0400

    Create Section class to manage books and availability

--- library_management.js
+++ library_management.js
@@ -6,7 +6,7 @@ class Book {
         this.author = author;
         this.ISBN = ISBN;
         this._isAvailable = true;
-    } //created book class
+    } //created book class with properties
 
 getDetails() {
     return (`Title: ${this.title} Author: ${this.author} ISBN: ${this.ISBN}`);
@@ -20,5 +20,32 @@ get isAvailable() {
 set isAvailable(statusUpdate) {
     this._isAvailable = statusUpdate
     //using the set method to update the status of the book
+}}
+
+
+
+//TASK 2 - Create a Section Class:
+
+class Section {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    } //created section class with properties 
+
+addBook(book) {
+    this.books.push(book);
+    //using the push method to push the book class into the empty array
 }
+
+getAvailableBooks() {
+    return this.books.filter(book => book.isAvailable).length;
+    //returns the total number of available books in the section
+}
+
+listBooks() {
+    return this.books.reduce((list, book) => {
+        return list + (`${book.title} - ${book.isAvailable ? "Available" : "Borrowed"}\n`);
+    }, "");
+    //using the reduce method to list all books in the section, to show title and availability
+  }   
 }
